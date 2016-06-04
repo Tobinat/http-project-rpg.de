97d55698fb1c198f94344d7d67ffe8f98e02b4d0
VariatoX <variatox@web.de>
2016-06-04 13:24:04 +0200

    >> DYNMARKET | Life_Server

--- life_server/config.cpp
+++ life_server/config.cpp
@@ -131,6 +131,17 @@ class CfgFunctions
 			class removeGang {};
 			class updateGang {};
 		};
+		
+        class DynMarket
+        {
+            file = "\life_server\Functions\DynMarket";
+            class calculatePrices {};
+            class config {};
+            class getUpdate {};
+            class HandleDB {};
+            class playerLogged {};
+            class sleeper {};
+        };
 	};
 
 	class ProjectRPG
