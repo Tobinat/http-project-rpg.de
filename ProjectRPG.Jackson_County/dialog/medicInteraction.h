71c756121d29887ad73b5727b715263d616b8429
VariatoX <variatox@web.de>
2016-06-18 14:22:14 +0200

    Optik

--- ProjectRPG.Jackson_County/dialog/medicInteraction.h
+++ ProjectRPG.Jackson_County/dialog/medicInteraction.h
@@ -162,7 +162,7 @@ class medicInteraction_Menu
 			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", 0.8};
 			text = "";
 			sizeEx = 0.025;
-			x = BGX + 0.03;
+			x = BGX + 0.29;
 			y = BGY + 0.32;
 			w = 0.24;
 			h = 0.038;
