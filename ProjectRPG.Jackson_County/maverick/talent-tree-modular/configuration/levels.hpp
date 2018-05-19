f0e58f2b38fa40c9ed5d5bb978fedfbca55c91f9
LeonardGoerlitz <leonard.goerlitz@t-online.de>
2018-05-19 07:02:30 +0200

    was ein dreck

--- ProjectRPG.Jackson_County/maverick/talent-tree-modular/configuration/levels.hpp
+++ ProjectRPG.Jackson_County/maverick/talent-tree-modular/configuration/levels.hpp
@@ -1,305 +1,611 @@
 class Level_0 {
     displayName = "Stufe 0";
     expRequired = 0;
-    perkPointsOnUnlock = 2;
+    perkPointsOnUnlock = 0;
 };
 
 class Level_1 {
     displayName = "Stufe 1";
-    expRequired = 85;
-    perkPointsOnUnlock = 2;
+    expRequired = 100;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_2 {
     displayName = "Stufe 2";
-    expRequired = 340;
-    perkPointsOnUnlock = 2;
+    expRequired = 300;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_3 {
     displayName = "Stufe 3";
-    expRequired = 765;
-    perkPointsOnUnlock = 2;
+    expRequired = 500;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_4 {
     displayName = "Stufe 4";
-    expRequired = 1360;
-    perkPointsOnUnlock = 2;
+    expRequired = 750;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_5 {
     displayName = "Stufe 5";
-    expRequired = 2125;
-    perkPointsOnUnlock = 2;
+    expRequired = 1465;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_6 {
     displayName = "Stufe 6";
-    expRequired = 3060;
-    perkPointsOnUnlock = 2;
+    expRequired = 2560;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_7 {
     displayName = "Stufe 7";
-    expRequired = 4165;
-    perkPointsOnUnlock = 2;
+    expRequired = 3200;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_8 {
     displayName = "Stufe 8";
-    expRequired = 5440;
-    perkPointsOnUnlock = 2;
+    expRequired = 4120;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_9 {
     displayName = "Stufe 9";
-    expRequired = 6885;
-    perkPointsOnUnlock = 2;
+    expRequired = 5290;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_10 {
     displayName = "Stufe 10";
-    expRequired = 8500;
+    expRequired = 6405;
     perkPointsOnUnlock = 2;
 };
 
 class Level_11 {
     displayName = "Stufe 11";
-    expRequired = 10285;
-    perkPointsOnUnlock = 2;
+    expRequired = 7810;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_12 {
     displayName = "Stufe 12";
-    expRequired = 12240;
-    perkPointsOnUnlock = 2;
+    expRequired = 9115;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_13 {
     displayName = "Stufe 13";
-    expRequired = 14365;
-    perkPointsOnUnlock = 2;
+    expRequired = 11450;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_14 {
     displayName = "Stufe 14";
-    expRequired = 16660;
-    perkPointsOnUnlock = 2;
+    expRequired = 14200;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_15 {
     displayName = "Stufe 15";
-    expRequired = 19125;
-    perkPointsOnUnlock = 2;
+    expRequired = 17250;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_16 {
     displayName = "Stufe 16";
-    expRequired = 21760;
-    perkPointsOnUnlock = 2;
+    expRequired = 21500;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_17 {
     displayName = "Stufe 17";
-    expRequired = 24565;
-    perkPointsOnUnlock = 2;
+    expRequired = 26250;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_18 {
     displayName = "Stufe 18";
-    expRequired = 27540;
-    perkPointsOnUnlock = 2;
+    expRequired = 30920;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_19 {
     displayName = "Stufe 19";
-    expRequired = 30685;
-    perkPointsOnUnlock = 2;
+    expRequired = 36550;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_20 {
     displayName = "Stufe 20";
-    expRequired = 34000;
+    expRequired = 43540;
     perkPointsOnUnlock = 2;
 };
 
 class Level_21 {
     displayName = "Stufe 21";
-    expRequired = 37485;
-    perkPointsOnUnlock = 2;
+    expRequired = 48299;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_22 {
     displayName = "Stufe 22";
-    expRequired = 41140;
-    perkPointsOnUnlock = 2;
+    expRequired = 54550;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_23 {
     displayName = "Stufe 23";
-    expRequired = 44965;
-    perkPointsOnUnlock = 2;
+    expRequired = 59990;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_24 {
     displayName = "Stufe 24";
-    expRequired = 48960;
-    perkPointsOnUnlock = 2;
+    expRequired = 64450;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_25 {
     displayName = "Stufe 25";
-    expRequired = 53125;
-    perkPointsOnUnlock = 2;
+    expRequired = 68900;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_26 {
     displayName = "Stufe 26";
-    expRequired = 57460;
-    perkPointsOnUnlock = 2;
+    expRequired = 74520;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_27 {
     displayName = "Stufe 27";
-    expRequired = 61965;
-    perkPointsOnUnlock = 2;
+    expRequired = 80000;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_28 {
     displayName = "Stufe 28";
-    expRequired = 66640;
-    perkPointsOnUnlock = 2;
+    expRequired = 86200;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_29 {
     displayName = "Stufe 29";
-    expRequired = 71485;
-    perkPointsOnUnlock = 2;
+    expRequired = 93250;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_30 {
     displayName = "Stufe 30";
-    expRequired = 76500;
+    expRequired = 100000;
     perkPointsOnUnlock = 2;
 };
 
 class Level_31 {
     displayName = "Stufe 31";
-    expRequired = 81685;
-    perkPointsOnUnlock = 2;
+    expRequired = 110000;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_32 {
     displayName = "Stufe 32";
-    expRequired = 87040;
-    perkPointsOnUnlock = 2;
+    expRequired = 120000;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_33 {
     displayName = "Stufe 33";
-    expRequired = 92565;
-    perkPointsOnUnlock = 2;
+    expRequired = 130000;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_34 {
     displayName = "Stufe 34";
-    expRequired = 98260;
-    perkPointsOnUnlock = 2;
+    expRequired = 140000;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_35 {
     displayName = "Stufe 35";
-    expRequired = 104125;
-    perkPointsOnUnlock = 2;
+    expRequired = 150000;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_36 {
     displayName = "Stufe 36";
-    expRequired = 110160;
-    perkPointsOnUnlock = 2;
+    expRequired = 160000;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_37 {
     displayName = "Stufe 37";
-    expRequired = 116365;
-    perkPointsOnUnlock = 2;
+    expRequired = 170000;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_38 {
     displayName = "Stufe 38";
-    expRequired = 122740;
-    perkPointsOnUnlock = 2;
+    expRequired = 180000;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_39 {
     displayName = "Stufe 39";
-    expRequired = 129285;
-    perkPointsOnUnlock = 2;
+    expRequired = 190000;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_40 {
     displayName = "Stufe 40";
-    expRequired = 136000;
+    expRequired = 200000;
     perkPointsOnUnlock = 2;
 };
 
 class Level_41 {
     displayName = "Stufe 41";
-    expRequired = 142885;
-    perkPointsOnUnlock = 2;
+    expRequired = 215000;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_42 {
     displayName = "Stufe 42";
-    expRequired = 149940;
-    perkPointsOnUnlock = 2;
+    expRequired = 230000;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_43 {
     displayName = "Stufe 43";
-    expRequired = 157165;
-    perkPointsOnUnlock = 2;
+    expRequired = 245000;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_44 {
     displayName = "Stufe 44";
-    expRequired = 164560;
-    perkPointsOnUnlock = 2;
+    expRequired = 260000;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_45 {
     displayName = "Stufe 45";
-    expRequired = 172125;
-    perkPointsOnUnlock = 2;
+    expRequired = 275000;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_46 {
     displayName = "Stufe 46";
-    expRequired = 179860;
-    perkPointsOnUnlock = 2;
+    expRequired = 290000;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_47 {
     displayName = "Stufe 47";
-    expRequired = 187765;
-    perkPointsOnUnlock = 2;
+    expRequired = 305000;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_48 {
     displayName = "Stufe 48";
-    expRequired = 195840;
-    perkPointsOnUnlock = 2;
+    expRequired = 320000;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_49 {
     displayName = "Stufe 49";
-    expRequired = 204085;
-    perkPointsOnUnlock = 2;
+    expRequired = 340000;
+    perkPointsOnUnlock = 1;
 };
 
 class Level_50 {
     displayName = "Stufe 50";
-    expRequired = 212500;
-    perkPointsOnUnlock = 2;
+    expRequired = 360000;
+    perkPointsOnUnlock = 3;
+};
+
+class Level_51 {
+    displayName = "Stufe 51";
+    expRequired = 380000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_52 {
+    displayName = "Stufe 52";
+    expRequired = 400000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_53 {
+    displayName = "Stufe 53";
+    expRequired = 420000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_54 {
+    displayName = "Stufe 54";
+    expRequired = 440000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_55 {
+    displayName = "Stufe 55";
+    expRequired = 460000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_56 {
+    displayName = "Stufe 56";
+    expRequired = 480000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_57 {
+    displayName = "Stufe 57";
+    expRequired = 500000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_58 {
+    displayName = "Stufe 58";
+    expRequired = 525000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_59 {
+    displayName = "Stufe 59";
+    expRequired = 550000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_60 {
+    displayName = "Stufe 60";
+    expRequired = 575000;
+    perkPointsOnUnlock = 3;
+};
+
+class Level_61 {
+    displayName = "Stufe 61";
+    expRequired = 600000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_62 {
+    displayName = "Stufe 62";
+    expRequired = 625000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_63 {
+    displayName = "Stufe 63";
+    expRequired = 650000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_64 {
+    displayName = "Stufe 64";
+    expRequired = 675000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_65 {
+    displayName = "Stufe 65";
+    expRequired = 700000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_66 {
+    displayName = "Stufe 66";
+    expRequired = 725000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_67 {
+    displayName = "Stufe 67";
+    expRequired = 750000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_68 {
+    displayName = "Stufe 68";
+    expRequired = 775000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_69 {
+    displayName = "Stufe 69";
+    expRequired = 800000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_70 {
+    displayName = "Stufe 70";
+    expRequired = 825000;
+    perkPointsOnUnlock = 3;
+};
+
+class Level_71 {
+    displayName = "Stufe 71";
+    expRequired = 850000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_72 {
+    displayName = "Stufe 72";
+    expRequired = 875000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_73 {
+    displayName = "Stufe 73";
+    expRequired = 900000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_74 {
+    displayName = "Stufe 74";
+    expRequired = 925000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_75 {
+    displayName = "Stufe 75";
+    expRequired = 950000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_76 {
+    displayName = "Stufe 76";
+    expRequired = 975000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_77 {
+    displayName = "Stufe 77";
+    expRequired = 1000000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_78 {
+    displayName = "Stufe 78";
+    expRequired = 1025000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_79 {
+    displayName = "Stufe 79";
+    expRequired = 1050000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_80 {
+    displayName = "Stufe 80";
+    expRequired = 1075000;
+    perkPointsOnUnlock = 3;
+};
+
+class Level_81 {
+    displayName = "Stufe 81";
+    expRequired = 1100000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_82 {
+    displayName = "Stufe 82";
+    expRequired = 1125000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_83 {
+    displayName = "Stufe 83";
+    expRequired = 1150000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_84 {
+    displayName = "Stufe 84";
+    expRequired = 1175000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_85 {
+    displayName = "Stufe 85";
+    expRequired = 1200000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_86 {
+    displayName = "Stufe 86";
+    expRequired = 1225000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_87 {
+    displayName = "Stufe 87";
+    expRequired = 1250000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_88 {
+    displayName = "Stufe 88";
+    expRequired = 1275000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_89 {
+    displayName = "Stufe 89";
+    expRequired = 1300000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_90 {
+    displayName = "Stufe 90";
+    expRequired = 1325000;
+    perkPointsOnUnlock = 3;
+};
+
+class Level_91 {
+    displayName = "Stufe 91";
+    expRequired = 1350000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_92 {
+    displayName = "Stufe 92";
+    expRequired = 1375000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_93 {
+    displayName = "Stufe 93";
+    expRequired = 1400000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_94 {
+    displayName = "Stufe 94";
+    expRequired = 1425000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_95 {
+    displayName = "Stufe 95";
+    expRequired = 1450000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_96 {
+    displayName = "Stufe 96";
+    expRequired = 1475000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_97 {
+    displayName = "Stufe 97";
+    expRequired = 1500000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_98 {
+    displayName = "Stufe 98";
+    expRequired = 1550000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_99 {
+    displayName = "Stufe 99";
+    expRequired = 1600000;
+    perkPointsOnUnlock = 1;
+};
+
+class Level_100 {
+    displayName = "Stufe 100";
+    expRequired = 1700000;
+    perkPointsOnUnlock = 5;
+};
+
+class Level_101 {
+    displayName = "Stufe 101";
+    expRequired = 5555555;
+    perkPointsOnUnlock = 20;
 };
