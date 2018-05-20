3b108b097f6639ff70a31cd6e75c31939d4b6017
Tobinat <tobias.lindemann@me.com>
2018-05-20 13:55:15 +0200

    Weg damit

--- ProjectRPG.Jackson_County/maverick/maverick_master.cpp
+++ ProjectRPG.Jackson_County/maverick/maverick_master.cpp
@@ -1,5 +1,6 @@
 #include "gui\includes.cpp"
 #include "talent-tree-modular\config.cpp"
 //#include "Shipwrecks\config.cpp"
-#include "navigation\config.cpp"
+/* #include "navigation\config.cpp"
 #include "taxis\config.cpp" 
+ */
