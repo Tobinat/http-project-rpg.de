c94a97dabb49161815850a16780f0af467596001
itskevinotte <itskevinotte@users.noreply.github.com>
2020-04-06 21:10:51 +0200

    Update localizationondutydystem
    Update localizationondutydystem

--- Übersetzungen/localizationdutysystem.hpp
+++ Übersetzungen/localizationdutysystem.hpp
@@ -2,161 +2,201 @@ class Prise_Service_Localization {
 	class STR_ALREADY_COP {
 		fr = "<t size='2' color='#49c3e5'>Service Policier</t><br/><br/> <t size='1,5'>Tu ne peux pas prendre ton service car tu es déjà en service</t>";
 		en = "<t size='2' color='#49c3e5'>Police Service </t><br/><br/> <t size='1,5'>You can't take your duty because you're already on duty</t>";
+		de = "<t size='2' color='#49c3e5'>Polizeidienst </t><br/><br/> <t size='1,5'>Sie können nicht in den Dienst, weil sie schon im Dienst sind</t>";
 	};
 	class STR_ALREADY_ADAC {
 		fr = "<t size='2' color='#996633'>Service Adac</t><br/><br/> <t size='1,5'>Tu ne peux pas prendre ton service car tu es déjà en service</t>";
 		en = "<t size='2' color='#996633'>Adac Service </t><br/><br/> <t size='1,5'>You can't take your duty because you're already on duty</t>";
+		de = "<t size='2' color='#996633'>Adac Service </t><br/><br/> <t size='1,5'>Sie können nicht in den Dienst, weil sie schon im Dienst sind</t>";
 	};
 	class STR_NOT_COP {
 		fr = "<t size='2' color='#49c3e5'>Service Policier</t><br/><br/> <t size='1,5'>Vous devez être titulaire du concours de la Policier pour rejoindre les forces de l'ordre !</t>";
 		en = "<t size='2' color='#49c3e5'>Police Service </t><br/><br/> <t size='1,5'>You must be a holder of the Police Officer's competition to join the police force !</t>";
+		en = "<t size='2' color='#49c3e5'>Polizeidienst </t><br/><br/> <t size='1,5'>Sie müssen Polizeibeamter sein, um in den Dienst zu treten !</t>";
 	};
 	class STR_NOT_ADAC {
 		fr = "<t size='2' color='#996633'>Service Adac</t><br/><br/> <t size='1,5'>Vous ne pouvez pas rejoindre les rangs des adacs !</t>";
 		en = "<t size='2' color='#996633'>Adac Service </t><br/><br/> <t size='1,5'>You can't join the adacs !</t>";
+		de = "<t size='2' color='#996633'>Adac Service </t><br/><br/> <t size='1,5'>Sie müssen beim Adac sein, um in den Dienst zu treten !</t>";
 	};
 	class STR_ONDUTTY_COP {
 		fr = "<t size='2' color='#49c3e5'>Service Policier</t><br/><br/><t size='1,5'>Félicitations, tu viens de prendre ton service en tant que policer</t>";
 		en = "<t size='2' color='#49c3e5'>Police Service </t><br/><br/><t size='1,5'>Congratulations, you just started your duty as a police officer</t>";
+		de = "<t size='2' color='#49c3e5'>Polizeidienst </t><br/><br/><t size='1,5'>Gratulation, Sie haben gerade Ihren Dienst als Polizist begonnen</t>";
 	};
 	class STR_ONDUTTY_ADAC {
 		fr = "<t size='2' color='#996633'>Service Adac</t><br/><br/><t size='1,5'>Félicitations, tu viens de prendre ton service en tant qu'adac</t>";
 		en = "<t size='2' color='#996633'>Adac Service </t><br/><br/><t size='1,5'>Congratulations, you just started your duty as adac</t>";
+		de = "<t size='2' color='#996633'>Adac Service </t><br/><br/><t size='1,5'>Gratulation, Sie haben gerade Ihren Dienst beim Adas begonnen</t>";
 	};
 	class STR_OFFDUTTY_COP {
 		fr = "<t size='2' color='#49c3e5'>Service Policier</t><br/><br/><t size='1,5'>Tu viens de quitter ton service en tant que policer</t>";
 		en = "<t size='2' color='#49c3e5'>Police Service </t><br/><br/><t size='1,5'>You just left your service as a police officer</t>";
+		de = "<t size='2' color='#49c3e5'>Polizeidienst </t><br/><br/><t size='1,5'>Sie haben den Dienst bei der Polizei beendet</t>";
 	};
 	class STR_OFFDUTTY_Adac {
 		fr = "<t size='2' color='#996633'>Service Adac</t><br/><br/><t size='1,5'>Tu viens de quitter ton service en tant qu'adac</t>";
 		en = "<t size='2' color='#996633'>Adac Service </t><br/><br/><t size='1,5'>You just left your service as adac</t>";
+		de = "<t size='2' color='#996633'>Adac Service </t><br/><br/><t size='1,5'>Sie haben den Dienst beim Adac beendet</t>";
 	};
 	class STR_TEXT_COP {
 		fr = "GENDARMERIE NATIONALE";
 		en = "LAW ENFORCEMENT";
+		de = "STRAFVERFOLGUNG";
 	};
 	class STR_NO_STOP_DUTY {
         fr = "%1 vient d'arreter son service !";
         en = "%1 has just stopped his duty !";
+		de = "%1 hat seinen Dienst beendet !";
     };
 	class STR_TEXT_EMT {
 		fr = "SAPEUR POMPIER";
 		en = "FIREFIGHTER";
+		de = "FEUERWEHRMANN";
 	};
 	class STR_TEXT_ADAC {
 		fr = "ADAC";
 		en = "ADAC";
+		de = "ADAC";
 	};
 	class STR_NO_DUTTY_Adac {
 		fr = "<t size='2' color='#996633'>Service Adac</t><br/><br/> <t size='1,5'>Tu ne peux pas quitter ton service alors que tu ne l'es pas...</t>";
 		en = "<t size='2' color='#996633'>Adac Service </t><br/><br/> <t size='1,5'>You can't leave your duty when you're not...</t>";
+		de = "<t size='2' color='#996633'>Adac Service </t><br/><br/> <t size='1,5'>Sie können den Dienst nicht beenden, wenn sie nicht...</t>";
 	};
 	class STR_NO_DUTTY_COP {
 		fr = "<t size='2' color='#49c3e5'>Service Policier</t><br/><br/> <t size='1,5'>Tu ne peux pas quitter ton service alors que tu ne l'es pas...</t>";
 		en = "<t size='2' color='#49c3e5'>Police Service </t><br/><br/> <t size='1,5'>You can't leave your duty when you're not...</t>";
+		en = "<t size='2' color='#49c3e5'>Polizeidienst </t><br/><br/> <t size='1,5'>Sie können den Dienst nicht beenden, wenn sie nicht...</t>";
 	};
 	class STR_NO_DUTTY_EMT {
 		fr = "<t size='2' color='#f40000'>Service Pompier</t><br/><br/> <t size='1,5'>Tu ne peux pas quitter ton service alors que tu ne l'es pas...</t>";
 		en = "<t size='2' color='#f40000'>Fire Department</t><br/><br/> <t size='1,5'>You can't leave your duty when you're not....</t>";
+		de = "<t size='2' color='#f40000'>Fire Department</t><br/><br/> <t size='1,5'>Sie können den Dienst nicht beenden, wenn sie nicht....</t>";
 	};
 	class STR_OFFDUTTY_EMT {
 		fr = "<t size='2' color='#f40000'>Service Pompier</t><br/><br/><t size='1,5'>Tu viens de quitter ton service en tant que pompier</t>";
 		en = "<t size='2' color='#f40000'>Fire Department</t><br/><br/><t size='1,5'>You just left your department as a firefighter</t>";
+		de = "<t size='2' color='#f40000'>Fire Department</t><br/><br/><t size='1,5'>Sie haben gerade Ihre Abteilung als Feuerwehrmann verlassen</t>";
 	};
 	class STR_ALREADY_EMT {
 		fr = "<t size='2' color='#f40000'>Service Pompier</t><br/><br/> <t size='1,5'>Tu ne peux pas prendre ton service car tu es déjà en service</t>";
 		en = "<t size='2' color='#f40000'>Fire Department</t><br/><br/> <t size='1,5'>You can't take your duty because you're already on duty</t>";
+		de = "<t size='2' color='#f40000'>Fire Department</t><br/><br/> <t size='1,5'>Sie können nicht in den Dienst, weil sie schon im Dienst sind</t>";
 	};
 	class STR_NOT_EMT {
 		fr = "<t size='2' color='#f40000'>Service Pompier</t><br/><br/> <t size='1,5'>Vous devez être titulaire de votre F.I.A pour rejoindre les pompiers</t>";
 		en = "<t size='2' color='#f40000'>Fire Department</t><br/><br/> <t size='1,5'>You must be a holder of the firefighters' contest to join the firefighters</t>";
+		de = "<t size='2' color='#f40000'>Fire Department</t><br/><br/> <t size='1,5'>Sie müssen bei der Feuerwehr sein, um in den Dienst zu treten</t>";
 	};
 	class STR_ONDUTTY_EMT {
 		fr = "<t size='2' color='#f40000'>Service Pompier</t><br/><br/> <t size='1,5'>Félicitations, tu viens de prendre ton service en tant que pompier</t>";
 		en = "<t size='2' color='#f40000'>Fire Department</t><br/><br/> <t size='1,5'>Congratulations, you have just started your duty as a firefighter.</t>";
+		de = "<t size='2' color='#f40000'>Fire Department</t><br/><br/> <t size='1,5'>Gratulation, Sie haben Ihren Dienst bei der Feuerwehrbegonnen.</t>";
 	};
 	class STR_BEEPER_MESSAGE {
 		fr = "<t size='2' color='#f40000'>Alerte Bipeur</t><br/><br/> <t size='1,5'>%1</t>";
 		en = "<t size='2' color='#f40000'>Beeper Alert</t><br/><br/> <t size='1,5'>%1</t>";
+		de = "<t size='2' color='#f40000'>Pager Alarm</t><br/><br/> <t size='1,5'>%1</t>";
 	};
 	class STR_NO_TEXT {
 		fr = "Vous n'avez pas mis de texte";
 		en = "You didn't put any text";
+		de = "Sie haben keinen Text eingefügt";
 	};
 	class STR_ENTER_MESSAGE {
 		fr = "Entrez votre message...";
 		en = "Enter your message...";
+		de = "Schreiben Sie eine Nachricht...";
 	};
 	class STR_MESSAGE_SEND {
 		fr = "Votre message a été envoyé !";
 		en = "Your message has been sent !";
+		de = "Ihre nachricht wurde versendet !";
 	};
 	class STR_MESSAGE_NOBODY {
 		fr = "Personne ne peut recevoir ton message";
 		en = "No one can receive your message";
+		de = "Niemand kann deine Nachricht erhalten";
 	};
 	class STR_NO_TAKE_DUTY {
 		fr = "%1 vient de prendre son service !";
 		en = "%1 has just taken up his duty !";
+		de = "%1 hat seinen Dienst begonnen !";
 	}
 	class STR_COP_ONLINE {
 		fr = "Policiers en ligne :";
 		en = "Online Police Officers :";
+		de = "Polizisten im Dienst :";
 	}; 
 	class STR_EMT_ONLINE {
 		fr = "Pompiers en ligne :";
 		en = "Online Firefighters :";
+		de = "Feuerwehrmänner/frauen im Dienst :";
 	};
 	class STR_ADAC_ONLINE {
 		fr = "Adac en ligne :";
 		en = "Adacs Online :";
+		de = "Adacs im Dienst :";
 	};
 	class STR_NO_COP_ONLINE {
 		fr = "Il n'y a aucun policier en ligne !";
 		en = "There are no police officers online !";
+		de = "Es befinden sich keine Polizisten im Dienst !";
 	};
 	class STR_NO_ADAC_ONLINE {
 		fr = "Il n'y a aucun adac en ligne !";
 		en = "There are no adac online !";
+		de = "Es befindet sich kein Adac-Mitarbeiter im Dienst !";
 	};
 	class STR_NO_EMT_ONLINE {
 		fr = "Il n'y a aucun pompier en ligne !";
 		en = "There are no firemen online  !";
+		de = "Es befindet sich niemand von der Feuerwehr im Dienst !";
 	};
 	class STR_AUTO_COP_DUTY {
 		fr = "Voulez vous rependre votre service en policer ?";
 		en = "Do you want to take over your duty as officer ?";
+		de = "Möchtest du deinen Dienst als Polizeibeamter starten ?";
 	};
 	class STR_AUTO_ADAC_DUTY {
 		fr = "Voulez vous rependre votre service en adac ?";
 		en = "Do you want to take over your duty as adac ?";
+		de = "Möchtest du deinen Dienst als Adac-Mitarbeiter starten ?";
 	};
 	class STR_AUTO_EMT_DUTY {
 		fr = "Voulez vous rependre votre service en pompier ?";
 		en = "Do you want to take over your duty as fire department ?";
+		de = "Möchtest du deinen Dienst bei der Feuerwehr starten ?";
 	};
 	class STR_DUTY_SYSTEM {
 		fr = "Prise de service";
 		en = "Service entry";
+		de = "Serviceeintrag";
 	};
 	class STR_DUTY_YES {
 		fr = "Oui";
 		en = "Yes";
+		de = "Ja";
 	};
 	class STR_DUTY_NO {
 		fr = "Non";
 		en = "No";
+		de = "Nein";
 	};
 	class STR_ACTION_MED {
 		fr = "Prise service Pompier";
 		en = "Firefighter service start";
+		de = "Feuerwehrdienst starten";
 	};
 	class STR_ACTION_COP {
 		fr = "Prise service Policer";
 		en = "Policer service start";
+		de = "Polizeidienst starten";
 	};
 	class STR_ACTION_ADAC {
 		fr = "Prise service Adac";
 		en = "Adac service start";
+		de = "Adac-Dienst starten";
 	};
 };
